c6c7210f1c329ad048fd2f5622a7ac305792950e
WooLegend <dnwogus4260@naver.com>
2023-09-13 23:22:32 +0900

    23.09.22

--- BAEKJOON/1753.cpp
+++ BAEKJOON/1753.cpp
@@ -5,16 +5,33 @@ using namespace std;
 
 int N, E, B, u, v, w;
 vector<pair<int, int>> ver[20001];
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+int min_dis[20001];
 
 void func(int p){
-    queue<int> q;
-    bool vis[N + 1];
-    int dis[N + 1];
-    fill(vis, vis + N + 1, false);
-    fill(dis, dis + N + 1, INF);
-    q.push(p);
+    fill(min_dis, min_dis + 20001, INF);
+    pq.push({0, p});
+    min_dis[p] = 0;
 
-    
+    while(!pq.empty()){
+        int dis =  pq.top().first;
+        int node = pq.top().second;
+        int len = ver[node].size();
+        pq.pop();
+        for(int i = 0; i < len; i++){
+            int next = ver[node][i].first;
+            int next_dis = dis + ver[node][i].second;
+            if(min_dis[next] > next_dis){
+                min_dis[next] = next_dis;
+                pq.push({next_dis, next});
+            }
+        }
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(min_dis[i] == INF) cout << "INF\n";
+        else cout << min_dis[i] << "\n";
+    }
 }
 
 int main(){
@@ -27,4 +44,6 @@ int main(){
         cin >> u >> v >> w;
         ver[u].push_back({v, w});
     }
+
+    func(B);
 }
